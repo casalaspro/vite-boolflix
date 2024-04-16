cad9147010a4562ba5af7b5a961d38c0b7f091a5
casalaspro <casalaspro.alessandro@gmail.com>
2024-04-16 12:40:52 +0200

    ADD - axios GET request

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import appSearch from './components/appSearch.vue'
 import { store } from './store'
+import axios from 'axios'
 
 export default{
   data(){
@@ -9,13 +10,27 @@ export default{
     }
   },
   methods:{
-    
+    fetchMovies(query){
+      axios.get('https://api.themoviedb.org/3/search/movie?', {
+        params:{
+          api_key: '8c5d2929491cc6bc1823b19280c48648',
+          query: query
+        }
+      })
+      .then((response)=>{
+        console.log(response);
+        store.movies.push(response.data.results);
+      })
+    }
   },
   components:{
     appSearch,
   },
   mounted(){
     console.log(store);
+    this.fetchMovies("Il signore degli");
+    console.log(store.movies);
+    
   }
 }
 </script>
